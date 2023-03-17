2e9d182c50df16680348584ae33d9a06ac527b49
HadirElnagdy <87231099+HadirElnagdy@users.noreply.github.com>
2023-03-17 16:06:17 +0200

    Initial commit: added project icon

---
+++ Etlas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Etlas
+//
+//  Created by Hadir on 17/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
